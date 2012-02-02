49447df97d7c9b583ca6cc3c818c7662bac5750f
Youssef <youssefouladfaris@gmail.com>
2012-02-02 14:54:35 +0100

    Initial check in

---
+++ specialdays.py
@@ -0,0 +1,40 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
+def daysalive(today, dayofbirth):
+    answer = today - dayofbirth
+    return answer.days
+         
+def inthefuture(today, dayofbirth, delta):
+    inthefuture = today + delta
+    return daysalive(inthefuture, dayofbirth)
+    
+def makerange(today, dob, delta):
+    return range(daysalive(today, dob), inthefuture(today, dob, delta))
+
+def primedays(today, dayofbirth, delta):
+    r = makerange(today, dayofbirth, delta)
+    return filter(isprime, r)
+
+
+        
---
+++ test_specialdays.py
@@ -0,0 +1,30 @@
+from specialdays import daysalive, inthefuture, makerange, primedays
+from datetime import date, timedelta
+
+
+def test_daysalive():
+    dob = date(1971,1,21)
+    today = date(1972,1,21)
+    assert daysalive(today, dob) == 365
+    
+
+def test_inthefuture():
+    dob = date(1971,1,21)
+    today = date(1971,1,31)
+    assert inthefuture(today,dob, timedelta(days=+5)) == 15
+    
+
+def test_range():
+    dob = date(1993,3,11)
+    today = date(2013,3,11)
+    r = makerange(today, dob, timedelta(days=+3652))
+    l = list(r)
+    assert len(l) == 3652
+    
+def test_primedays():
+    dob = date(2000,1,1)
+    today = date(2000,1,11)
+    r = primedays(today, dob, timedelta(days=+10))
+    l = list(r)
+    assert l == [11,13,17,19]
+    
